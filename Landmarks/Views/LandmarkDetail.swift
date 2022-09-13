a9630b7f098a8eda7f825a3536bc6709c784e818
Ashutosh Mukherjee <ashutosh.mukherjee@tadigital.com>
2022-09-13 18:10:42 +0530

    Done till lesson 2 section 6

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -1,13 +1,13 @@
 //
-//  ContentView.swift
+//  LandmarkDetail.swift
 //  Landmarks
 //
-//  Created by Ashutosh Mukherjee on 12/09/22.
+//  Created by Ashutosh Mukherjee on 13/09/22.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandmarkDetail: View {
     var body: some View {
         VStack {
             MapView()
@@ -46,8 +46,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandmarkDetail()
     }
 }
