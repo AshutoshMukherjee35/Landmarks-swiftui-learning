66cb110324c8e1d2fb036225d7ccb1042a08aa0a
Ashutosh Mukherjee <ashutosh.mukherjee@tadigital.com>
2022-09-14 12:35:38 +0530

    completed handling user input done with SWIFTUI essentials.

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -4,14 +4,17 @@
 //
 //  Created by Ashutosh Mukherjee on 12/09/22.
 //
-
 import SwiftUI
 
 @main
 struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
+
